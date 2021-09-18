b862c6ec5f145d08b8e64909bdfcacd2214d5751
katgzco <katgzco@gmail.com>
2021-09-18 22:08:19 +0000

    Feature: add stats route for show the class objects number

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""  """
+"""contains the paths and views used by the server"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 status = Flask(__name__)
 
@@ -11,3 +18,17 @@ def status():
     """ Return in JSON data"""
     data = {'status': 'OK'}
     return jsonify(data)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Return in JSON data"""
+    classes = {"Amenity": Amenity, "City": City,
+               "Place": Place, "Review": Review, "State": State, "User": User}
+
+    count_class = dict()
+
+    for clas, objclass in classes.items():
+        count_class.update({clas: storage.count(objclass)})
+
+    return jsonify(count_class)
