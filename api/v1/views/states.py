d70405a58bc9664e38ef21d823fc3c0929777963
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-19 22:45:35 +0000

    Fixing code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Create a new view for State objects that handles all default RESTFul API actions"""
+""" Create a new view for State
+objects that handles all default RESTFul API actions"""
 
 from models import storage
 from flask import Flask, jsonify, request, make_response, abort
 from api.v1.views import app_views
 from models.state import State
+
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
@@ -15,6 +17,7 @@ def states():
         states_list.append(states.to_dict())
     return jsonify(states_list)
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def states_id(state_id):
     """ Return one element of State matched with id"""
@@ -23,16 +26,19 @@ def states_id(state_id):
         return abort(404)
     return jsonify(object.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                methods=['DELETE'], strict_slashes=False)
 def states_id_delete(state_id):
     """ Return one element of State matched with id"""
     object = storage.get(State, state_id)
     if object is None:
-        return page_not_found(404)
+        abort(404)
     else:
         storage.delete(object)
         storage.save()
-        return jsonify({}), 200
+        return {}, 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -49,12 +55,13 @@ def create_state():
         abort(400, 'Not a JSON')
     return make_response(jsonify(instance.to_dict()), 201)
 
-@app_views.route('states/<state_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('states/<string:state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """  one element of State matched with id"""
     object = storage.get(State, state_id)
     if object is None:
-        return abort(404)
+        abort(404)
     else:
         if request.is_json:
             variable = request.get_json(request.data)
