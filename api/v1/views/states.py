fcbc2583b7dd94a1a5a2c6a44242e9cb46a3f87b
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-19 22:55:31 +0000

    Fixing aborts

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,16 +43,16 @@ def states_id_delete(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """ Create a new State"""
-    if request.is_json:
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    else:
         variable = request.get_json(request.data)
         if 'name' in variable.keys():
             instance = State(name = variable['name'])
             storage.new(instance)
             storage.save()
         else:
-            abort(400, 'Missing name')
-    else:
-        abort(400, 'Not a JSON')
+            abort(400, 'Missing name')        
     return make_response(jsonify(instance.to_dict()), 201)
 
 
@@ -63,11 +63,13 @@ def update_state(state_id):
     if object is None:
         abort(404)
     else:
-        if request.is_json:
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        else:
             variable = request.get_json(request.data)
             if 'name' in variable.keys():
                 object.name = variable['name']
                 storage.save()
             else:
-                abort(400, 'Not a JSON')
+                abort(400, 'Missing name')         
     return jsonify(object.to_dict()), 200
