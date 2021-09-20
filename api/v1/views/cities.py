76a4cf1c49f523737eabf06b7eda6b13884a661f
katgzco <katgzco@gmail.com>
2021-09-20 20:22:36 +0000

    Fix: change logic in state validation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,13 +13,14 @@ from flask import jsonify, request, make_response, abort
                  methods=['GET'], strict_slashes=False)
 def all_cities(state_id):
     """ Return the list of all cities"""
+    object = storage.get(State, state_id)
+    if object is None:
+        abort(404)
     all_objects = storage.all(City)
     cities_list = []
     for cities in all_objects.values():
         if state_id == cities.state_id:
             cities_list.append(cities.to_dict())
-    if len(cities_list) == 0:
-        abort(404)
     return jsonify(cities_list)
 
 
